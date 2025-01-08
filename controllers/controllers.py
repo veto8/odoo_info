98b74e09744a46a1fa91a5365c410bb829bc8bee
veto <veto@myridia.com>
2025-01-08 18:30:20 +0700

    WorkAgain

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class OdooInfo(http.Controller):
-#     @http.route('/odoo_info/odoo_info', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class OdooInfo(http.Controller):
+    @http.route('/odoo_info/odoo_info', auth='public')
+    def index(self, **kw):
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        return "Hello, world"
 
-#     @http.route('/odoo_info/odoo_info/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('odoo_info.listing', {
-#             'root': '/odoo_info/odoo_info',
-#             'objects': http.request.env['odoo_info.odoo_info'].search([]),
-#         })
+    @http.route('/odoo_info/odoo_info/objects', auth='public')
+    def list(self, **kw):
+        return http.request.render('odoo_info.listing', {
+            'root': '/odoo_info/odoo_info',
+            'objects': http.request.env['odoo_info.odoo_info'].search([]),
+        })
 
-#     @http.route('/odoo_info/odoo_info/objects/<model("odoo_info.odoo_info"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('odoo_info.object', {
-#             'object': obj
-#         })
+    @http.route('/odoo_info/odoo_info/objects/<model("odoo_info.odoo_info"):obj>', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('odoo_info.object', {
+            'object': obj
+        })
 
